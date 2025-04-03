3908ed800a5a74276c691dc9632d639473d60dac
Lesh <162008740+Marshall-Israel-Omingo@users.noreply.github.com>
2025-04-03 11:04:35 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. leshram israel
+            Save and see your changes instantly. leshram israelbbbbbbbbb
           </li>
         </ol>
 
